7cc7d378fd0e3b9c9175c82b75b4c5a80ed75e87
Sergei Buiko <sergeibuiko1@gmail.com>
2022-08-05 01:42:26 +0300

    feat: add algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
+const one = ['','one','two','three','four','five','six','seven','eight','nine'];
+const ten = ['','','twenty','thirty', 'forty','fifty','sixty','seventy','eighty','ninety']   
+const teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+
+function toHundered (number) {
+    if (number >= 100) {
+        return (one[Math.floor(number / 100)] + ' hundred ' + toTeens(number % 100)).trim();
+    }
+    return toNull(number)
 }
+function toTeens (number) {
+    if (number >= 0 && number < 10) {
+        return one[number] } 
+    else if ( number >= 10 && number < 20) {
+        return teens[number % 10]
+    }
+    else if (number >=20 && number < 100)
+    return (ten[Math.floor(number / 10)] + ' ' + one[number % 10]).trim() 
+    }
+    return toHundered(number)
+
+function toNull (number) {
+    if (number === 0) {
+        return 'zero'
+    }
+    return toTeens(number)
+}
+}
+
+
+
+
